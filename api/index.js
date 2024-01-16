9995374145c6c29b8c0f6baa46a29d6c3739a950
jeel33 <jmpatel332000@gmail.com>
2024-01-15 21:51:25 -0500

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
+mongoose.connect(process.env.MONGO_URI).then((data)=>{
+    console.log(`mongodb connected successfully ${data.connection.host}`)
+}).catch((err)=>{
+    console.log(err)
+})
 
 app.listen(3000, ()=>{
     console.log("server is listening on 3000")
